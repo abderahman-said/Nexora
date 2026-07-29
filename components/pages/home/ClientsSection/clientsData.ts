1af4f6326b49e910b6de13cf44c6bb8d56ad3e61
abderahman-said <abdorady6500@gmail.com>
2026-07-30 00:56:32 +0300

    feat: add Abdelrahman Mokhtar testimonial data and associated avatar image

--- components/pages/home/ClientsSection/clientsData.ts
+++ components/pages/home/ClientsSection/clientsData.ts
@@ -7,6 +7,6 @@ export const TESTIMONIALS: Testimonial[] = [
         role: "Client",
         comment: "A highly professional company with an exceptional and respectful development team. I've worked with them on more than three software projects, and every one of them was a complete success. I highly recommend working with them and choosing them for your next project.",
         rating: 5,
-        avatar: "/clients/client_1.png",
+        avatar: "/clients/abdo.png",
     }
 ];
