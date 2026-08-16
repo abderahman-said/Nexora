5a6ea8e4c93dfbe8582167a24ffaf08cabe6ded2
abderahman-said <abdorady6500@gmail.com>
2026-08-16 10:47:51 +0300

    feat: add AboutVisionMission component to home page section

--- components/pages/home/AboutSection/AboutVisionMission.tsx
+++ components/pages/home/AboutSection/AboutVisionMission.tsx
@@ -8,7 +8,7 @@ export default function AboutVisionMission() {
     <div className="w-full pt-4">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12">
         <div className="space-y-4">
-          <h3 className="text-xl font-extrabold text-slate-900 dark:text-white">
+          <h3 className="text-xl font-extrabold  text-sky-600 dark:text-sky-400 ">
             {t("vision_title")}
           </h3>
           <p className="max-w-lg text-sm sm:text-base text-slate-600 dark:text-slate-300 leading-relaxed font-normal">
@@ -16,7 +16,7 @@ export default function AboutVisionMission() {
           </p>
         </div>
         <div className="space-y-4">
-          <h3 className="text-xl font-extrabold text-slate-900 dark:text-white">
+          <h3 className="text-xl font-extrabold  text-sky-600 dark:text-sky-400 ">
             {t("mission_title")}
           </h3>
 
